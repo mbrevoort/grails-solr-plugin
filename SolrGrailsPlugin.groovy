8f6fda0fd4704e170587fbe089ab4ba55d95ce99
Robin Bramley <robin@rbramley.com>
2012-02-24 17:27:50 +0000

    Updated version

--- SolrGrailsPlugin.groovy
+++ SolrGrailsPlugin.groovy
@@ -38,7 +38,7 @@ import org.grails.solr.SolrUtil
 
 class SolrGrailsPlugin {
     // the plugin version
-    def version = "0.3"
+    def version = "0.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
